ed9b7672c3feed87c9945a14e9b28d4fb43636cd
vinej <jyvinet@hotmail.ca>
2016-07-01 19:34:13 -0400

    styles

--- src/components/header.js
+++ src/components/header.js
@@ -2,8 +2,8 @@ import Inferno from 'inferno'
 
 export let Header = () => 
   <div className="pure-g header">
-      <div className="pure-u-1-2"><div>Inferno-Mobx-Starter</div></div>
-      <div className="pure-u-1-2">
+      <div className="pure-u-1-4"><div>InfernoMobxStarter</div></div>
+      <div className="pure-u-3-4">
         <div
             onClick= { () => alert('Mobx example') } 
             style={{float: 'right'}}>?</div></div>
