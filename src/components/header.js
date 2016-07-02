13f9318cf2d3909843f0b2549eafcd954a67215d
Jean-Yves Vinet <jyvinet@hotmail.ca>
2016-07-02 17:33:14 -0400

    Update header.js

--- src/components/header.js
+++ src/components/header.js
@@ -5,8 +5,8 @@ export let Header = () =>
   <div className="pure-g header">
       <div className="pure-u-1-2"><div>InfernoMobxStarter</div></div>
       <div className="pure-u-1-2" >
-        <Link className='mlink' to='/todos' >Todos</Link>
-        <Link className='mlink' to='/welcome' >Welcome</Link>
+        <Link to='/todos' >Todos</Link>
+        <Link to='/welcome' >Welcome</Link>
         <div
             onClick= { () => alert('Mobx example') } 
             style={{float: 'right'}}>?</div></div>
